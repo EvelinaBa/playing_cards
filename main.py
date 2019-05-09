a1e04b4f010d46810c157e819c9b8cc1be8df05c
EvelinaBa <evelina.bezubec@gmail.com>
2019-05-09 20:30:37 +0300

    fix #1

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("Hello world")
+import random
 
+suits = ["spade","club","heart","diamond"]
+rank = [2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"]
 
+suit1 = random.choice(suit)
+rank1 = random.choice(rank)
+
+card1 = ("spade", 2)
+all_cards = []
+
+for suit in suits:
+    for card in cards:
+        card = (suit, card)
+        all_cards.append(card)
+print(all_cards)
