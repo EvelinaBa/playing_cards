c66ebd0c29b7732e1386565980ef45792b3c4fd1
EvelinaBa <evelina.bezubec@gmail.com>
2019-05-09 20:43:11 +0300

    fix #2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
+from random import shuffle
 
 suits = ["spade","club","heart","diamond"]
 rank = [2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"]
 
-suit1 = random.choice(suit)
-rank1 = random.choice(rank)
+# suit1 = random.choice(suit)
+# rank1 = random.choice(rank)
 
 card1 = ("spade", 2)
 all_cards = []
@@ -13,4 +14,6 @@ for suit in suits:
     for card in cards:
         card = (suit, card)
         all_cards.append(card)
+
+shuffle(all_cards)
 print(all_cards)
