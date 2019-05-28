c0b855c76f2e9c8d6178b1919ccb54f526dd244e
EvelinaBa <evelina.bezubec@gmail.com>
2019-05-28 16:36:47 +0300

    fix #4

--- main.py
+++ main.py
@@ -1,34 +1,90 @@
-import random
-
-cards = []
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-ranks = ["Jack", "Queen", "King", "Ace"]
-deck = []
-
-for i in range(2,11):
-    cards.append(str(i))
+class Card(object):
+    RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
     
-for r in range(4):
-    cards.append(ranks[r])
-    
-for k in range(4):
-    for l in range(13):
-        card = (cards[l] + " of " + suits[k])
-        deck.append(card)
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.value = self.RANKS.index(rank) + 1
 
-random.shuffle(deck)
-    
-for d in range(52):
-    print(deck[d])
-    
-player1_hand = []
-player2_hand = []
+    def __str__(self):
+        rep = self.rank + self.suit
+        return rep
 
-n = 2
-l = deck
+    def __cmp__(self, other):
+        return cmp(self.value, other.value)
 
-def chunks(l, n):
-    n = max(1, n)
-    return (l[i:i+n] for i in range(0, len(l), n))
+class Hand(object):
 
-list(chunks(l=deck, n=2))
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep += str(card) + " "
+        else:
+            rep = ""
+        return rep
+
+    def clear(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def give(self, card, other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
+
+class Deck(Hand):
+        
+    def populate(self):
+        for suit in Card.SUITS:
+            for rank in Card.RANKS:
+                self.add(Card(rank, suit))
+
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
+
+    def deal(self, hand, per_hand = 1):
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards[0]
+                    self.give(top_card, hand)
+                else:
+                    print("Out of cards!")
+
+def cardvalue(cr):
+    if cr == "Ace":
+        value = 1
+    elif cr == "2":
+        value = 2
+    elif cr == "3":
+        value = 3
+    elif cr == "4":
+        value = 4
+    elif cr == "5":
+        value = 5
+    elif cr == "6":
+        value = 6
+    elif cr == "7":
+        value = 7
+    elif cr == "8":
+        value = 8
+    elif cr == "9":
+        value = 9
+    elif cr == "10":
+        value = 10
+    elif cr == "Jack":
+        value = 11
+    elif cr == "Queen":
+        value = 12
+    elif cr == "King":
+        value = 13
+    else:
+        value = 0
+    return value
