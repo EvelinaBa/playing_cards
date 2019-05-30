104983dda683b05d82c1067cb2e2b620eaa49d13
EvelinaBa <evelina.bezubec@gmail.com>
2019-05-30 21:28:20 +0300

    added logging

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import random
 import itertools
+import logging
+
+logging.basicConfig(
+    level='DEBUG',
+    filename='app.log'
+)
 
 # Create a shuffled deck of cards
-cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10','Jack', 'Queen', 'King', 'Ace']
+cards = ['2', '3', '4', '5', '6', '7', '8',
+         '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 deck = list(itertools.product(cards, suits))
 random.shuffle(deck)
@@ -12,6 +19,8 @@ player1_hand = deck[:len(deck)//2]
 player2_hand = deck[len(deck)//2:]
 
 # Define card values
+
+
 def cardvalue(cr):
     if cr == 'Ace':
         value = 1
@@ -43,6 +52,7 @@ def cardvalue(cr):
         raise "unknown value"
     return value
 
+
 # Count how many rounds a player wins
 win_count1 = 0
 win_count2 = 0
@@ -51,25 +61,25 @@ win_count2 = 0
 # Reveal a card for each player until cards run out and compare the values of the cards
 for i in range(len(player1_hand)):
 
-    print(player1_hand[i], ' vs ', player2_hand[i])
+    logging.info('%s %s', player1_hand[i], player2_hand[i])
 
     if cardvalue(player1_hand[i][0]) > cardvalue(player2_hand[i][0]):
         win_count1 += 1
-        print('Player1 wins!')
+        logging.info('Player1 wins!')
     elif cardvalue(player1_hand[i][0]) == cardvalue(player2_hand[i][0]):
-        print('Draw!')
+        logging.info('Draw!')
     else:
         win_count2 += 1
-        print('Player2 wins!')
+        logging.info('Player2 wins!')
 
 # Reveal the total count of wins for each player
-print('Player1 win count:', win_count1) 
-print('Player2 win count:', win_count2)
+logging.info('Player1 win count: %s', win_count1)
+logging.info('Player2 win count: %s', win_count2)
 
 # Determine the winner of the game
 if win_count1 > win_count2:
-    print('Player1 wins the game!')
+    logging.info('Player1 wins the game!')
 elif win_count1 == win_count2:
-    print('Draw!')
+    logging.info('Draw!')
 else:
-    print('Player2 wins the game!')
+    logging.info('Player2 wins the game!')
