31ac5be930fb8bc56859815e78afbc711f457ba8
EvelinaBa <evelina.bezubec@gmail.com>
2019-05-30 18:16:12 +0300

    fix #7

--- main.py
+++ main.py
@@ -1,111 +1,83 @@
-class Card(object):
-    RANKS = ["Ace of ", "2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ", "9 of ", "10 of ", "Jack of ", "Queen of ", "King of "]
-    SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
-        self.value = self.RANKS.index(rank) + 1
+import random
+import itertools
 
-    def __str__(self):
-        rep = self.rank + self.suit
-        return rep
 
-    def __cmp__(self, other):
-        return cmp(self.value, other.value)
+deck = list(itertools.product(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'],['Clubs', 'Diamonds', 'Hearts', 'Spades']))
+random.shuffle(deck)
+print(deck)
 
-class Hand(object):
 
-    def __init__(self):
-        self.cards = []
+print(len(deck))
 
-    def __str__(self):
-        if self.cards:
-            rep = ""
-            for card in self.cards:
-                rep += str(card) + " "
-        else:
-            rep = ""
-        return rep
+#Deal cards to each player
+player1_hand = deck[:len(deck)//2]
+player2_hand = deck[len(deck)//2:]
 
-    def clear(self):
-        self.cards = []
+# One card is revealed for each player
 
-    def add(self, card):
-        self.cards.append(card)
-
-    def give(self, card, other_hand):
-        self.cards.remove(card)
-        other_hand.add(card)
-
-class Deck(Hand):
-        
-    def populate(self):
-        for suit in Card.SUITS:
-            for rank in Card.RANKS:
-                self.add(Card(rank, suit))
-
-    def shuffle(self):
-        import random
-        random.shuffle(self.cards)
-
-    def deal(self, hand, per_hand = 1):
-        for rounds in range(per_hand):
-            for hand in hands:
-                if self.cards:
-                    top_card = self.cards[0]
-                    self.give(top_card, hand)
-                else:
-                    print("Out of cards!")
+#Values are compared
 
 def cardvalue(cr):
-    if cr == "Ace of ":
-        value = 1
-    elif cr == "2 of ":
-        value = 2
-    elif cr == "3 of ":
-        value = 3
-    elif cr == "4 of ":
-        value = 4
-    elif cr == "5 of ":
-        value = 5
-    elif cr == "6 of ":
-        value = 6
-    elif cr == "7 of ":
-        value = 7
-    elif cr == "8 of ":
-        value = 8
-    elif cr == "9 of ":
-        value = 9
-    elif cr == "10 of ":
-        value = 10
-    elif cr == "Jack of ":
-        value = 11
-    elif cr == "Queen of ":
-        value = 12
-    elif cr == "King of ":
-        value = 13
-    else:
-        value = 0
-    return value
+ if cr == 'Ace':
+     value = 1
+ elif cr == '2':
+     value = 2
+ elif cr == '3':
+     value = 3
+ elif cr == '4':
+     value = 4
+ elif cr == '5':
+     value = 5
+ elif cr == '6':
+     value = 6
+ elif cr == '7':
+     value = 7
+ elif cr == '8':
+     value = 8
+ elif cr == '9':
+     value = 9
+ elif cr == '10':
+     value = 10
+ elif cr == 'Jack':
+     value = 11
+ elif cr == 'Queen':
+     value = 12
+ elif cr == 'King':
+     value = 13
+ else:
+     raise "unknown value"
+ return value
 
-deck1 = Deck()
-deck1.populate()
-deck1.shuffle()
-player1_hand = Hand()
-player2_hand = Hand()
-hands = [player1_hand, player2_hand]
-deck1.deal(hands, per_hand=1)
 
-print("Player1 hand:", player1_hand)
-print("Player2 hand:", player2_hand)
+#cardvalue(deck[2][0]) < cardvalue(deck[4][0])
 
-player1_val = cardvalue(player1_hand.cards[0].rank)
-player2_val = cardvalue(player2_hand.cards[0].rank)
+win_count1 = 0
+win_count2 = 0
 
-if player1_val > player2_val:
-    print("\nPlayer1 wins!")
-elif player1_val == player2_val:
-    print("\nDraw!")
+for i in range(len(player1_hand)):
+ #cardvalue(player1_hand[i][0])
+ #cardvalue(player2_hand[i][0])
+
+ #print(cardvalue(player1_hand[i][0]))
+ #print(cardvalue(player2_hand[i][0]))
+
+ print('\n' ,player1_hand[i], ' vs ', player2_hand[i])
+
+
+ if cardvalue(player1_hand[i][0]) > cardvalue(player2_hand[i][0]):
+   win_count1 +=1
+   print('Player1 wins\n')
+ elif cardvalue(player1_hand[i][0]) == cardvalue(player2_hand[i][0]):
+   print('Draw\n')
+ else:
+   win_count2 +=1
+   print('Player2 wins\n')
+
+print(win_count1, win_count2)
+
+if win_count1 > win_count2:
+ print('pl1 wins')
+elif win_count1 == win_count2:
+ print('draw')
 else:
-    print("\nPlayer2 wins!")
+ print('pl2 wins')
