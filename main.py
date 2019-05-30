6faf1787e300eca40bf2401a4ff06c4bca84cb41
EvelinaBa <evelina.bezubec@gmail.com>
2019-05-30 20:33:26 +0300

    fixed format

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import random
 import itertools
 
-#Create a shuffled deck of cards
+# Create a shuffled deck of cards
 deck = list(itertools.product(['2', '3', '4', '5', '6', '7', '8', '9', '10',
                                'Jack', 'Queen', 'King', 'Ace'], ['Clubs', 'Diamonds', 'Hearts', 'Spades']))
 random.shuffle(deck)
-print(deck)
 
-#Split deck into two parts
+# Split deck into two parts
 player1_hand = deck[:len(deck)//2]
 player2_hand = deck[len(deck)//2:]
 
-#Define card calues
+# Define card values
 def cardvalue(cr):
     if cr == 'Ace':
         value = 1
@@ -43,33 +42,35 @@ def cardvalue(cr):
         raise "unknown value"
     return value
 
-#Count how many rounds a player wins
+
+# Count how many rounds a player wins
 win_count1 = 0
 win_count2 = 0
 
 
-#Reveal a card for each player until cards run out and compare the values of the cards
+# Reveal a card for each player until cards run out and compare the values of the cards
 for i in range(len(player1_hand)):
 
-    print('\n', player1_hand[i], ' vs ', player2_hand[i])
+    print(player1_hand[i], ' vs ', player2_hand[i])
 
     if cardvalue(player1_hand[i][0]) > cardvalue(player2_hand[i][0]):
         win_count1 += 1
-        print('Player1 wins!\n')
+        print('Player1 wins!')
     elif cardvalue(player1_hand[i][0]) == cardvalue(player2_hand[i][0]):
-        print('Draw!\n')
+        print('Draw!')
     else:
         win_count2 += 1
-        print('Player2 wins!\n')
+        print('Player2 wins!')
 
 
-#Reveal the total count of wins for each player
-print(win_count1, win_count2)
+# Reveal the total count of wins for each player
+print('Player1 win count:', win_count1) 
+print('Player2 win count:', win_count2)
 
-#Determine the winner of the game
+# Determine the winner of the game
 if win_count1 > win_count2:
-    print('Player1 wins!')
+    print('Player1 wins the game!')
 elif win_count1 == win_count2:
     print('Draw!')
 else:
-    print('Player2 wins!')
+    print('Player2 wins the game!')
